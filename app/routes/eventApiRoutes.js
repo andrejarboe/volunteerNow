c22b750f9fb689a459d8425278c4b5a25aebb66e
andrejarboe <andrejarboe@gmail.com>
2018-01-16 19:43:34 -0500

    add get route for all events from a specific organization

--- app/routes/eventApiRoutes.js
+++ app/routes/eventApiRoutes.js
@@ -12,11 +12,18 @@ var db = require("../models");
 // =============================================================
 module.exports = function (app) {
 
+  //make routes for:
+//   □ All
+//   □ One 
+//   □ Org
+//   □ Time
+// skills
+
   // get routue for all the events avaliable 
   app.get("/api/events", function (req, res) {
     query.OrganizationId = req.query.organization_id;
 
-    db.Events.findAll({
+    db.Events.findOne({
       include: [ db.Organization ],
       where: query
     }).then(function (dbPost) {
@@ -24,5 +31,16 @@ module.exports = function (app) {
     });
 
   });
+
+  // get route for all events from a specific organization
+  app.get("/api/events/organization/:organization", function (req, res){
+    db.Event.findAll({
+      where: {
+        organization: req.params.organization
+      }
+    }).then(function(dbEvent){
+
+    });
+  })
 
 }
