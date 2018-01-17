532326acfc6362de623b750335b3661f41cfbbda
Pete Fullen <pfullen.code@gmail.com>
2018-01-16 22:41:21 -0500

    added associations

--- models/user.js
+++ models/user.js
@@ -17,6 +17,7 @@ module.exports = (sequelize, DataTypes) => {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        User.hasMany(model.UserTimeSheet)
       }
     }
   });
--- models/usertimesheet.js
+++ models/usertimesheet.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+          UserTimeSheet.belongsTo(model.User);
       }
     }
   });
