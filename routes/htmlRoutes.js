aac42822abf01de67e1d5cf1f74b21e801e6b71f
andrejarboe <andrejarboe@gmail.com>
2018-01-22 19:17:25 -0500

    add routes for all organizations and all opportunities

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,15 +13,15 @@ var db = require("../models");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
     // index route loads index page
-    app.get("/", function(req, res) {
+    app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
     //user sign up forum
-    app.get("/signup", function(req, res) {
+    app.get("/signup", function (req, res) {
         //   var fileContents = fs.readFileSync('./public/javascript/volunteer.js');
         // res.write(fileContents);
         // If the user already has an account send them to the login page
@@ -31,28 +31,52 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/signup.html"));
     });
 
-    app.get("/login", function(req, res) {
-         if (req.user) {
+    app.get("/login", function (req, res) {
+        if (req.user) {
             res.redirect("/");
         }
         res.sendFile(path.join(__dirname, "../public/login.html"));
     });
 
     //organization sign up forum
-    app.get("/profile", isAuthenticated, function(req, res) {
+    app.get("/profile", isAuthenticated, function (req, res) {
 
         res.sendFile(path.join(__dirname, "../public/user.html"));
     });
 
-      /* handlebars
-  *****************************************/
-  //html route to display all users
-  app.get("/users", function(req, res) {
-    db.User.findAll({}).then(function(dbUsers) {
-      res.render("users", { usersData: dbUsers });
+    /* handlebars
+    *****************************************/
+    //html route to display all users
+    app.get("/users", function (req, res) {
+        db.User.findAll({}).then(function (dbUsers) {
+            res.render("users", { usersData: dbUsers });
+        });
     });
-  });
 
+    // html route to display one user
+    app.get("/user/:id", function (req, res) {
+        db.User.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbUser) {
+            res.render("user", { user: dbUser });
+        });
+    });
+
+    //html route to display all organizations
+    app.get("/organizations", function (req, res) {
+        db.Organization.findAll({}).then(function (dbOrganizations) {
+            res.render("organizations", { organizations: dbOrganizations });
+        });
+    });
+
+    //html route to display all Opportunities
+    app.get("/opportunities", function (req, res) {
+        db.Opportunity.findAll({}).then(function (dbOpportunity) {
+            res.render("opportunity", { opportunity: dbOpportunity });
+        });
+    });
 
 
 };
