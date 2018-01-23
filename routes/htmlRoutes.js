f7475a41b98ad8682d6fb7f827e820dcf0248e76
andrejarboe <andrejarboe@gmail.com>
2018-01-22 19:07:36 -0500

    add route for /users

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,10 +5,11 @@
 // Dependencies
 // =============================================================
 var path = require("path");
-// var fs = require('fs');
-// // var sequelizeRouter = require('sequelize-router');
+var fs = require('fs');
+var sequelizeRouter = require('sequelize-router');
 // var usersData = require("../data/user");
 var isAuthenticated = require("../config/middleware/isAuthenticated");
+var db = require("../models");
 
 // Routes
 // =============================================================
@@ -43,63 +44,15 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/user.html"));
     });
 
-
-    //     app.get("/test", function(req, res) {
-
-    //     });
-
-    //     //one user
-    //     app.get("/user/:id", function(req, res) {
-    //         var id = req.params.id - 1;
-    //         res.render("user", usersData[id]);
-
-    //         console.log(usersData[0]);
-
-    //     });
-
-    //     // Here we've add our isAuthenticated middleware to this route.
-    //     // If a user who is not logged in tries to access this route they will be redirected to the signup page
+      /* handlebars
+  *****************************************/
+  //html route to display all users
+  app.get("/users", function(req, res) {
+    db.User.findAll({}).then(function(dbUsers) {
+      res.render("users", { usersData: dbUsers });
+    });
+  });
 
 
-
-
-    //     //org 
-
-    //     /* handlebars
-    //      *****************************************/
-    //     //store user data
-    //     //push data to route
-    //     app.get("/users", function(req, res) {
-    //         res.render("users", { usersData });
-    //     });
-
-
-    //     //one 
-    //     //org 
-
-    //     //one org
-
-    //     //events
-
-    //     // events route loads the event(s)
-    //     app.get("/events/:id ", function(req, res) {
-    //         res.sendFile(path.join(__dirname, "../views/partials/events.html"));
-    //     });
-
-    //     //create event route loads post event page
-    //     app.get("/events/create", function(req, res) {
-    //         res.sendFile(path.join(__dirname, "../views/partials/createEvent.html"));
-    //     });
-
-
-    //     //Organization profile route loads organization profile(s)
-    //     app.get("/organization/:id ", function(req, res) {
-    //         res.sendFile(path.join(__dirname, "../views/partials/organization.html"));
-    //     });
-
-    //     //User profile route loads user profile
-    //     app.get("/user-profile/:id ", function(req, res) {
-    //         res.sendFile(path.join(__dirname, "../views/partials/userProfile.html"));
-    //     });
 
 };
